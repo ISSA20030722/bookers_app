cd2337b24b9337a1efd654fd02496793bdfb4e63
EC2 Default User <ec2-user@ip-172-31-38-173.ap-northeast-1.compute.internal>
2025-04-13 01:44:43 +0000

    Initial commit: 最初の設定からresoucesのルーティングまで

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # トップ画面をhomes#topに設定する
+  root 'homes#top'
+  resouces :books
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
